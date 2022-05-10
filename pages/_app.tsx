3859dda19c44404abae808d2761969193777b78b
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-10 23:52:06 +0200

    added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/tailwind.css';
+import Head from 'next/head'
 
 import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <title>Kacke Ballern</title>
+        <Component {...pageProps} />
+      </>
+  )
 }
 
 export default App
