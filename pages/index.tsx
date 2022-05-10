1a0a5f1ee892dadecab734f5051d6f182f37553a
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-11 00:41:44 +0200

    added responsive styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,32 +14,32 @@ const Home: NextPage = () => {
               <div className='flex flex-row justify-evenly p-5 text-3xl'>
                   <h1 className='text-kb-white text-4xl lg:text-7xl'>Kacke Ballern</h1>
               </div>
-              <div className='flex flex-row justify-evenly p-5'>
+              <div className='flex flex-col md:flex-row justify-evenly p-5'>
                   <div>
                     <h1 className='text-center text-kb-white text-2xl'>Ausbringmenge</h1>
-                    <div className='flex flex-row'>
+                    <div className='flex flex-row justify-center'>
                         <p style={{background: 'none'}} className='text-kb-white border-2 appearance-none border-2 text-8xl border-gray-200 rounded w-48 py-2 text-center leading-tight focus:outline-none focus:bg-white'>{ausbringmenge}</p>
                     </div>
                   </div>
                   <div>
                     <h1 className='text-center text-kb-white text-2xl'>Geschwindigkeit</h1>
-                      <div className='flex flex-row'>
+                      <div className='flex flex-row justify-center'>
                         <button className='text-6xl text-kb-white' onClick={() => setGeschwindigkeit(geschwindigkeit - 1)}>-</button>
                         <input width={2} style={{background: 'none'}} className='text-kb-white border-t-0 border-x-0 border-b-2 appearance-none border-2 text-8xl border-gray-200 rounded w-48 py-2 text-center leading-tight focus:outline-none focus:bg-white' type="number" value={geschwindigkeit} onChange={(e) => setGeschwindigkeit(parseFloat(e.target.value))}/>
                         <button className='text-4xl text-kb-white' onClick={() => setGeschwindigkeit(geschwindigkeit + 1)}>+</button>
                       </div>
                   </div>
               </div>
-              <div className='flex flex-row justify-evenly p-5'>
+              <div className='flex flex-col md:flex-row justify-evenly p-5'>
                   <div>
                     <h1 className='text-center text-kb-white text-2xl'>Reichweite</h1>
-                      <div className='flex flex-row'>
+                      <div className='flex flex-row justify-center'>
                         <p style={{background: 'none'}} className='text-kb-white border-2 appearance-none border-2 text-8xl border-gray-200 rounded w-48 py-2 text-center leading-tight focus:outline-none focus:bg-white'>{reichweite}</p>
                       </div>
                   </div>
                   <div>
                     <h1 className='text-center text-kb-white text-2xl'>Zapfwellendrehzahl</h1>
-                      <div className='flex flex-row'>
+                      <div className='flex flex-row justify-center'>
                         <button className='text-6xl text-kb-white' onClick={() => setZapfwellendrehzahl(zapfwellendrehzahl - 1)}>-</button>
                         <input width={2} style={{background: 'none'}} className='text-kb-white border-t-0 border-x-0 border-b-2 appearance-none border-2 text-8xl border-gray-200 rounded w-48 py-2 text-center leading-tight focus:outline-none focus:bg-white' type="number" value={zapfwellendrehzahl} onChange={(e) => setZapfwellendrehzahl(parseFloat(e.target.value))}/>
                         <button className='text-4xl text-kb-white' onClick={() => setZapfwellendrehzahl(zapfwellendrehzahl + 1)}>+</button>
