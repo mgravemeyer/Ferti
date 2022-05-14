8dcf2e88e5cee8ed653fc68a824d2d30153316e9
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-14 21:33:29 +0200

    added button click animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,11 @@ const SelectionButton = ({onClickAction, element, selection}: SelectionButtonPro
             style={props}
             onMouseEnter={() => set({transform: `scale(${ 1.1 })`, boxShadow: `0px ${'10px 20px'} 0px rgba(0, 0, 0, 0.10)`})}
             onMouseLeave={() => set({transform: `scale(${ 1 })`, boxShadow: `0px ${'5px 15px'} 0px rgba(0, 0, 0, 0.10)`})}
+            onMouseDown={() => set({transform: `scale(${ 1.0 })`, boxShadow: `0px ${'10px 20px'} 0px rgba(0, 0, 0, 0.10)`})}
+            onMouseUp={() => set({transform: `scale(${ 1.1 })`, boxShadow: `0px ${'10px 20px'} 0px rgba(0, 0, 0, 0.10)`})}
             onClick={onClickAction}
             key={element}
-            className={`py-1 px-2 cursor-pointer ${element === selection ? 'bg-kb-green-dark text-kb-white' : 'bg-kb-white text-kb-green-dark'}`}
+            className={`py-1 px-2 cursor-pointer select-none ${element === selection ? 'bg-kb-green-dark text-kb-white' : 'bg-kb-white text-kb-green-dark'}`}
         >
             <p>{element}</p>
         </animated.div>
