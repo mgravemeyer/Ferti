0c892cc5cd10e12d24987c6c0a660ab2eb5e82e6
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-14 12:56:14 +0200

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,21 @@ import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
+
+  const favIconPath = '/favicon';
+
   return (
       <>
-        <title>Gülle-Nährstoffe</title>
+        <Head>
+            <title>Gülle-Nährstoffe</title>
+            <link rel="apple-touch-icon" sizes="76x76" href={`${favIconPath}/apple-touch-icon.png`}/>
+            <link rel="icon" type="image/png" sizes="32x32" href={`${favIconPath}/favicon-32x32.png`}/>
+            <link rel="icon" type="image/png" sizes="16x16" href={`${favIconPath}/favicon-16x16.png`}/>
+            <link rel="manifest" href={`${favIconPath}/site.webmanifest`}/>
+            <link rel="mask-icon" href={`${favIconPath}/safari-pinned-tab.svg`} color="#5bbad5"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="theme-color" content="#ffffff"/>
+        </Head>
         <Component {...pageProps} />
       </>
   )
