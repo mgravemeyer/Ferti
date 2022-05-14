c51d7594b50cff58cf2797c4d25e4561ef5aa054
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-14 20:51:04 +0200

    increased drop-shadow

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,15 +14,15 @@ type SelectionButtonProps = {
 const SelectionButton = ({onClickAction, element, selection}: SelectionButtonProps) => {
     const [props, set] = useSpring(() => ({
         transform: `scale(1)`,
-        boxShadow: `0px 5px 15px 0px rgba(0, 0, 0, 0.07)`,
+        boxShadow: `0px 5px 15px 0px rgba(0, 0, 0, 0.10)`,
         config: { tension: 400, mass: 2, velocity: 5 }
     }))
 
     return (
         <animated.div
             style={props}
-            onMouseEnter={() => set({transform: `scale(${ 1.1 })`, boxShadow: `0px ${'10px 20px'} 0px rgba(0, 0, 0, 0.07)`})}
-            onMouseLeave={() => set({transform: `scale(${ 1 })`, boxShadow: `0px ${'5px 15px'} 0px rgba(0, 0, 0, 0.07)`})}
+            onMouseEnter={() => set({transform: `scale(${ 1.1 })`, boxShadow: `0px ${'10px 20px'} 0px rgba(0, 0, 0, 0.10)`})}
+            onMouseLeave={() => set({transform: `scale(${ 1 })`, boxShadow: `0px ${'5px 15px'} 0px rgba(0, 0, 0, 0.10)`})}
             onClick={onClickAction}
             key={element}
             className={`py-1 px-2 cursor-pointer ${element === selection ? 'bg-kb-green-dark text-kb-white' : 'bg-kb-white text-kb-green-dark'}`}
