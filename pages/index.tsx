62e47180526d53bcc1a18156097121249a0bfdfa
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-14 21:10:48 +0200

    added spring animation to increase and decrease buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -259,7 +259,21 @@ type AddButtonProps = {
   unit: string,
 }
 
+const AddButtonAnimatedButton = () => {
+
+}
+
 const AddButton = ({label, get, add, reduce, change, className, floatDigits, unit}: AddButtonProps) => {
+
+    const [propsMinus, setMinus] = useSpring(() => ({
+      transform: `scale(1)`,
+      config: { tension: 400, mass: 2, velocity: 5 }
+    }))
+
+    const [propsPlus, setPlus] = useSpring(() => ({
+      transform: `scale(1)`,
+      config: { tension: 400, mass: 2, velocity: 5 }
+    }))
 
     return (
       <div className={className}>
@@ -268,9 +282,26 @@ const AddButton = ({label, get, add, reduce, change, className, floatDigits, uni
             <h1 className='text-center text-kb-green-dark md:pt-0'>{`(${unit})`}</h1>
         </div>
           <div className='flex flex-row justify-center'>
-            <button className='text-6xl text-kb-green-dark px-3' onClick={() => reduce()}>-</button>
+
+            <animated.div
+                style={propsMinus}
+                onMouseEnter={() => setMinus({transform: `scale(${ 1.2 })`})}
+                onMouseLeave={() => setMinus({transform: `scale(${ 1 })`})}
+                className='flex items-center text-6xl text-kb-green-dark pr-3 pl-6  hover:cursor-pointer select-none'
+                onClick={() => reduce()}
+            >
+                -
+            </animated.div>
             <input pattern="\d*" width={2} style={{background: 'none'}} className='text-kb-green-dark border-t-0 border-x-0 border-b-2 appearance-none border-2 text-8xl border-gray-200 w-56 text-center leading-tight focus:outline-none focus:bg-white' type="number" value={get.toFixed(floatDigits)} onChange={(e) => change(parseFloat(e.target.value))}/>
-            <button className='text-4xl text-kb-green-dark px-3' onClick={() => add()}>+</button>
+            <animated.div
+                style={propsPlus}
+                onMouseEnter={() => setPlus({transform: `scale(${ 1.1 })`})}
+                onMouseLeave={() => setPlus({transform: `scale(${ 1 })`})}
+                className='flex items-center text-6xl text-kb-green-dark pr-3 pl-6  hover:cursor-pointer select-none'
+                onClick={() => add()}
+            >
+                +
+            </animated.div>
           </div>
       </div>
     )
