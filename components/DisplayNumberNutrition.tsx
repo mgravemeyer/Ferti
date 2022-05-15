88f2e91dd9ecf093260fb7c7a0780c3a5fc418a8
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-15 02:45:22 +0200

    added test prefix

--- components/DisplayNumberNutrition.tsx
+++ components/DisplayNumberNutrition.tsx
@@ -11,19 +11,19 @@ const DisplayNumberNutrition = ({label, get, old, unit, className, icon, prefix}
           </div>
           <div className='flex flex-col justify-center'>
             <div className='flex flex-row items-baseline'>
-              <AnimatedNumber get={get} oldInput={old} prefix={prefix}/>&nbsp;
+              <AnimatedNumber get={get} oldInput={old} prefix={'test'}/>&nbsp;
               <p className='text-kb-green-dark'>{'N Gesamt'}</p>
             </div>
             <div className='flex flex-row items-baseline'>
-              <AnimatedNumber get={get} oldInput={old} prefix={prefix}/>&nbsp;
+              <AnimatedNumber get={get} oldInput={old} prefix={'test2'}/>&nbsp;
               <p className='text-kb-green-dark'>{'NH4-N'}</p>
             </div>
             <div className='flex flex-row items-baseline'>
-              <AnimatedNumber get={get} oldInput={old} prefix={prefix}/>&nbsp;
+              <AnimatedNumber get={get} oldInput={old} prefix={'test3'}/>&nbsp;
               <p className='text-kb-green-dark'>{'P2O5'}</p>
             </div>
             <div className='flex flex-row items-baseline'>
-              <AnimatedNumber get={get} oldInput={old} prefix={prefix}/>&nbsp;
+              <AnimatedNumber get={get} oldInput={old} prefix={'test4'}/>&nbsp;
               <p className='text-kb-green-dark'>{'K2O'}</p>
             </div>
             <p className='text-kb-green-dark'>{label}</p>
