c6bf65b36c788b24ae60b3c667e82a1d3b57e35d
mgravemeyer <m.gravemeyer@icloud.com>
2022-05-15 15:07:37 +0200

    added ferti icon

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,36 +1,17 @@
+import Image from "next/image";
+import copyIcon from "../public/ferti-icon.png";
+
 const Navigation = () => {
     return (
         <nav className="bg-white border-gray-200 rounded dark:bg-gray-800 pb-12">
-            <div className="flex flex-wrap justify-between items-center">
-                <a href="" className="flex items-center">
-                    <img src="" className="mr-3 h-6 sm:h-9" alt=""/>
+            <div className="flex flex-wrap items-center">
+                <a href="" className="flex items-center hover:underline">
+                    <div className='flex items-center h-full w-12'>
+                        <Image alt='icon' src={copyIcon} loading={'eager'}/>
+                    </div>
                     <span
-                        className="hover:underline self-center text-xl font-semibold whitespace-nowrap text-kb-green-dark">Gülle Rechner</span>
+                        className="pt-1 pl-1 self-center text-xl font-semibold whitespace-nowrap text-kb-green-dark">Ferti App</span>
                 </a>
-                <button data-collapse-toggle="mobile-menu" type="button"
-                        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="mobile-menu" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                              clipRule="evenodd"></path>
-                    </svg>
-                    <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                              clipRule="evenodd"></path>
-                    </svg>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                        <li>
-                            <a href="#"
-                               className="hover:underline block py-2 pr-4 pl-3 text-kb-green-dark border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Über mich</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     )
